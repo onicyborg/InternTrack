bebc7aebb9ed1ef9b9c5bf5d6c503a8ff64c78b3
Ace <akhmadfauzy40@gmail.com>
2025-08-29 09:54:52 +0700

    add: middleware role

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,19 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::prefix('admin')->middleware('role:admin')->group(function () {
+
+});
+
+Route::prefix('dosen')->middleware('role:dosen')->group(function () {
+
+});
+
+Route::prefix('pembina')->middleware('role:pembina')->group(function (){
+
+});
+
+Route::prefix('mahasiswa')->middleware('role:mahasiswa')->group(function (){
+
+});
