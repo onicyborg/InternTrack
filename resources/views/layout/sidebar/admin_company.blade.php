b2fd73081f82eb1b689ad95785d11ebe16c9d5f9
Ace <akhmadfauzy40@gmail.com>
2025-09-07 00:06:45 +0700

    update: route logout

--- resources/views/layout/sidebar/admin_company.blade.php
+++ resources/views/layout/sidebar/admin_company.blade.php
@@ -105,7 +105,7 @@
 
                 <!--begin:Menu item-->
                 <div class="menu-item">
-                    <a class="menu-link" href="/logout">
+                    <a class="menu-link" href="{{ route('logout') }}">
                         <span class="menu-bullet"><span class="bullet bullet-dot"></span></span>
                         <span class="menu-title">Logout</span>
                     </a>
